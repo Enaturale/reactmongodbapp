9671f54120c89955dc559e78164f006e1b1b1c6a
Enaturale <enaturaleb@gmail.com>
2022-12-19 15:04:32 +0100

    multer and sharp for upload and resize

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,33 @@
 //creating private routes
-exports.isAuth = (req, res, next) => {
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+exports.isAuth = async (req, res, next) => {
+    if(req.headers && req.headers.authorization){
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            const user = User.findById(decode.userId);
+
+            if(!user){
+                return res.json({sucess: false, message:"Unauthorized access!"});
+            }
+
+            req.user = user;
+            next()
+        } catch (error) {
+            if(error.name === 'JsonWebTokenError'){
+                return res.json({sucess: false, message: 'Unauthorized access!'})
+            }
+            if(error.name === 'TokenExpiredError' ){
+                return res.json({sucess: false, message: 'Session expired'})                
+            }
+
+            return res.json({sucess: false, message: 'Internal Server Error'})   
+            
+        }
+    }else{
+        return res.json({sucess: false, message:"Unauthorized access!"});
+    }
 
 }
--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const {createUser, userSignIn} = require('../controller/user');
+const { createUser, userSignIn } = require('../controller/user');
+const { isAuth } = require('../middleware/auth');
 const { validateUserSignUp, userValidation, validateUserSignIn } = require('../middleware/Validation/user');
+const User = require("../models/user")
 
 //router for user Signup
 router.post(
@@ -13,7 +15,46 @@ router.post(
       createUser);
 
 //router for signin
-router.post('/sign-in',validateUserSignIn, userValidation, userSignIn);
+router.post('/sign-in', validateUserSignIn, userValidation, userSignIn);
 
+//we need multer to iupload images
+const multer = require('multer');
+const storage = multer.memoryStorage();
+
+//we need sharp to resize images
+const sharp = require('sharp');
+
+//to check if the file is an image file
+const fileFilter = (req, res, cb) => {
+      if (file.mimetype.startsWith('image')) {
+            cb(null, true)
+      } else {
+            cb("Invalid image file!", false)
+      }
+}
+const uploads = multer({ storage, fileFilter })
+
+//router for uploading image
+router.post('/upload-image', isAuth, uploads.single('profile'), async (req, res) => {
+      const { user } = req
+
+      if (!user) return res.status(401).json({ sucess: false, message: 'Unauthorized' })
+
+      try {
+            const profileBuffer = req.file.buffer
+            const { width, height } = await sharp(profileBuffer).metadata()
+            const finalProfileImage = await sharp(profileBuffer).resize(Math.round(width * 0.5), Math.round(height * 0.5)).toBuffer()
+
+            await User.findByIdAndUpdate(user._id, { profileimage: finalProfileImage })
+
+            res.status(201).json({success: true, message:"Profile is updated"})
+      } catch (error) {
+            res.status(500).json({success: false, message:"Server error"})
+
+            console.log(error.message);
+      }
+
+
+})
 
 module.exports = router
